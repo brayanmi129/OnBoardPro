9ff445e669d527def990c5f88fc304ea9d2d7f1a
Brayan Miranda <brakcris129@gmail.com>
2024-10-25 00:31:29 -0500

    path public

--- index.js
+++ index.js
@@ -3,19 +3,10 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  const htmlResponse = `
-    <html>
-      <head>
-        <title>NodeJs y Express en Vercel</title>
-      </head>
-      <body>
-        <h1>Soy un proyecto Back end en vercel</h1>
-      </body>
-    </html>
-  `;
-  res.send(htmlResponse);
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html'); 
 });
+
 
 app.listen(port, () => {
   console.log(`port runing in http://localhost:${port}`);
