ccaaa63a35ad8e02e8dc3b748df32f49612eedad
Brayan Miranda <brakcris129@gmail.com>
2024-10-25 00:29:15 -0500

    path prod10

--- index.js
+++ index.js
@@ -1,47 +1,22 @@
-// express
-const express = require('express');
-const session = require('express-session');
-const passport = require('passport');
-const port = process.env.PORT || 3000;
+const express = require("express");
 const app = express();
 
-//firebase
-const { conectfirebase } = require('./api-ext/firebase/firebaseinit.js');
-const functions = require('firebase-functions');
-conectfirebase();
+const port = process.env.PORT || 3000;
 
-//google
-app.use(session({ secret: 'studify2024', resave: false, saveUninitialized: true }));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-//routes
-const userRoutes = require('./routes/userroutes.js');
-const authRoutes = require('./routes/authRoutes.js');
-
-
-//middlewares para poder recibir datos json
-app.use(express.json());
-app.use(express.static('public'));
-
-
-const cors = require('cors');
-
-app.use(cors({
-    origin: '*', // Permitir solo este origen
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
-    allowedHeaders: ['Content-Type'], // Encabezados permitidos
-}));
-
-app.use('/api/users', userRoutes);
-app.use('/api/auth', authRoutes);
-
-app.get('/*', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html'); 
+app.get("/", (req, res) => {
+  const htmlResponse = `
+    <html>
+      <head>
+        <title>NodeJs y Express en Vercel</title>
+      </head>
+      <body>
+        <h1>Soy un proyecto Back end en vercel</h1>
+      </body>
+    </html>
+  `;
+  res.send(htmlResponse);
 });
 
 app.listen(port, () => {
-    console.log('Servidor iniciado en http://localhost:3000');
+  console.log(`port runing in http://localhost:${port}`);
 });
-
